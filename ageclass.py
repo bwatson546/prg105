39d6a00e8c74129e17a57e04912c78def73672b7
bwatson546 <44175566+bwatson546@users.noreply.github.com>
2019-02-25 11:01:48 -0600

    Add files via upload

---
+++ ageclass.py
@@ -0,0 +1,14 @@
+age = int(input("How OLD is your HUMAN?"))
+if age <= 0:
+    print("This is GESTATING. Please COME BACK LATER.")
+elif age > 0 and  age <= 1:
+    print("That is an INFANT. It cannot TELL JOKES.")
+elif age > 1 and age < 13:
+    print("That is a dang CHILD. It tells TOO MANY JOKES.")
+elif age > 13 and age < 20:
+    print("Why have you brought this TEEN before me? I do not WANT IT.")
+elif age > 10 and age < 500:
+    print("This is a GROWN-ASS ADULT. It CAN MAKE IT ON ITS OWN.")
+elif age >= 5000000:
+    print("Please remove your ELDER GOD. I am FRIGHTENED of it.")
+
