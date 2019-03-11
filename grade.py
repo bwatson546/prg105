082910ab307f086a41ebfd9b7fc51583a501b2a1
bwatson546 <44175566+bwatson546@users.noreply.github.com>
2019-03-11 10:58:51 -0500

    Add files via upload

--- grade.py
+++ grade.py
@@ -1,28 +1,30 @@
 def main():
-    determine_grade()
+    average = calc_average()
+    determine_grade(average)
 
 
 def calc_average():
+    print("Please input your scores out of 100.")
     score1 = input("Test One Score: ")
     score2 = input("Test Two Score: ")
     score3 = input("Test Three Score: ")
     score4 = input("Test Four Score: ")
     score5 = input("Test Five Score: ")
-    average = score1 + score2 + score3 + score4 + score5
+    average = (float(score1) + float(score2) + float(score3) + float(score4) + float(score5)) / 5
     return average
 
 
-def determine_grade():
-    score = calc_average()
-    if score >= 90:
+def determine_grade(score):
+    print(score)
+    if float(score) >= 90:
             print("You got an A! The only grade that matters, according to jerks!!")
-    elif score < 90 >= 80:
+    elif float(score) < 90 and score >= 80:
             print("You got a B! Plenty good, means you're still learning!")
-    elif score < 80 >= 70:
+    elif float(score) < 80 and score >= 70:
             print("You got a C! Well! Sure!")
-    elif score < 70 >= 60:
+    elif float(score) < 70 and score >= 60:
             print("You got a D! Maybe, uh, school better!")
-    elif score < 60:
+    elif float(score) < 60:
             print("You got an F! If you find a good enough marker, that's a pretty easy B to fake!")
 
 
